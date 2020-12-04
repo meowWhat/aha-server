4e21a674a00a52b117201098f910c2ff68ea276f
cjh <1797793818@qq.com>
2020-12-04 23:01:11 +0800

    - change port to 30001

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const FS = FileStore(session)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const port = 3000
+  const port = 30001
   // ip 请求次数限制
   app.use(
     rateLimit({
