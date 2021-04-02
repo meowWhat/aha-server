88d127daa632d7f7fe541c819de1cb2ec992205d
cjh <1797793818@qq.com>
2021-04-02 18:48:47 +0800

    - 更新好友表

--- src/modules/Friend/Friend.controller.ts
+++ src/modules/Friend/Friend.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Session, Post, Put, Body, Delete } from '@nestjs/common'
 import { userAdderStore } from 'src/db/globalStore'
-import { USER, USER_ACCOUNT, USER_RELATION } from 'src/db/tables'
+import { USER, USER_ACCOUNT, USER_FRIENDS } from 'src/db/tables'
 import { createOne, deleteOne, findByCondition, result, updateOne } from 'src/helper/sqlHelper'
 import { addPrefix, getRandom, getUseridBySessionKey } from 'src/helper/utils'
 import { MySession } from 'src/type'
@@ -15,9 +15,8 @@ export class FriendController {
   async getFriends(@Session() { userKey }: MySession) {
     try {
       const userId = getUseridBySessionKey(userKey)
-      const row1 = await findByCondition(USER_RELATION, { [this.friend_id]: userId }, [this.user_id])
-      const row2 = await findByCondition(USER_RELATION, { [this.user_id]: userId }, [this.friend_id])
-      return result([...row1, ...row2], 200)
+      const row1 = await findByCondition(USER_FRIENDS, { id: userId })
+      return result(row1, 200)
     } catch (error) {
       return result(error)
     }
@@ -30,22 +29,29 @@ export class FriendController {
     const checkRow = (row: any) => {
       return Array.isArray(row) && row.length === 1 && row[0].id
     }
+    const userId = getUseridBySessionKey(userKey)
 
     try {
       const row1 = await findByCondition(USER_ACCOUNT, { email }, ['id'])
       if (checkRow(row1)) {
-        // 好友账号 ID
         const fAccountId = row1[0].id
-        // 好友 userID
         const row2 = await findByCondition(USER, { [addPrefix(USER_ACCOUNT)]: fAccountId }, ['id'])
 
         if (checkRow(row2)) {
+          // 获取好友 id
           const fUserId = row2[0].id
-          const random = getRandom(10)
-          const userId = getUseridBySessionKey(userKey)
-          if (userId === fUserId) {
-            return result('不能添加自己为好友！')
+          const row = await findByCondition(USER_FRIENDS, { [this.friend_id]: fUserId })
+
+          if (row.length > 0) {
+            return result('好友已添加!')
           }
+          // 生成密钥
+          const random = getRandom(10)
+
+          if (userId === fUserId) {
+            return result('不能添加自己为好友!')
+          }
+
           // 储存添加状态 
           userAdderStore.set(random, userId)
 
@@ -64,14 +70,19 @@ export class FriendController {
   @Post()
   async createFriend(@Body('key') key: string, @Session() { userKey }: MySession) {
     try {
-      const fUserId = getUseridBySessionKey(userKey)
-      const userId = userAdderStore.get(key)
+      const userId = getUseridBySessionKey(userKey)
+      const friendId = userAdderStore.get(key)
+
       if (!userId) {
         return result('好友添加已过期')
       }
+
       // 添加好友
-      await createOne(USER_RELATION, { [this.user_id]: userId, [this.friend_id]: fUserId })
+      await createOne(USER_FRIENDS, { id: userId, [this.friend_id]: friendId })
+      await createOne(USER_FRIENDS, { id: friendId, [this.friend_id]: userId })
+
       userAdderStore.delete(userId)
+
       return result('好友添加成功', 200)
     } catch (error) {
       return result(error)
@@ -82,14 +93,12 @@ export class FriendController {
   @Put()
   async updateFriend(@Session() { userKey }: MySession, @Body() body: {}) {
     try {
-      if (body[this.friend_id] || body[this.user_id] || body['id']) {
+      if (body[this.friend_id] || body['id']) {
         return result('参数错误,用户id不能修改')
       }
-      // 权限问题 [ todo ]
-
       const userId = getUseridBySessionKey(userKey)
-      await updateOne(USER_RELATION, { [this.friend_id]: userId }, body)
-      await updateOne(USER_RELATION, { [this.user_id]: userId }, body)
+
+      await updateOne(USER_FRIENDS, { [this.friend_id]: userId }, body)
 
       return result('好友添加成功', 200)
     } catch (error) {
@@ -102,8 +111,8 @@ export class FriendController {
   async deleteFriend(@Session() { userKey }: MySession, @Body('friend_id') fUserId: string) {
     try {
       const userId = getUseridBySessionKey(userKey)
-      await deleteOne(USER_RELATION, { [this.user_id]: userId, [this.friend_id]: fUserId })
-      await deleteOne(USER_RELATION, { [this.friend_id]: userId, [this.user_id]: fUserId })
+      await deleteOne(USER_FRIENDS, { id: userId, [this.friend_id]: fUserId })
+      await deleteOne(USER_FRIENDS, { id: fUserId, [this.friend_id]: userId })
       return result('好友删除成功', 200)
     } catch (error) {
       return result(error)
